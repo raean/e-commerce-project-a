658939f80fb5db272c79f9c3e74774a2b3f43312
Ragheb Abunahla <ragheban@gsp08.eecs.yorku.ca>
2018-09-11 18:21:41 -0400

    second commit

--- src/a/TCPServer.java
+++ src/a/TCPServer.java
@@ -1,7 +1,13 @@
 package a;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;	// Effective class for storing file names and paths.
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.net.InetAddress;	// Represents IP addresses and has methods to retrieve hostnames.
 import java.net.ServerSocket;	// This class implements server sockets. A server socket waits for requests to come in over the network. It performs some operation based on that request, and then possibly returns a result to the requester. 
@@ -11,7 +17,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TCPServer {
-	Set<InetAddress> whitelist;
+	private Set<InetAddress> whitelist;
 	
 	public TCPServer(int port, PrintStream log, File file) throws Exception {
 		
@@ -23,23 +29,31 @@ public class TCPServer {
 		
 		while (file.exists()) {
 			Socket client = server.accept(); // A socket is an end-point for communication between two machines.
-			// Check the client's IP and filter it (?) - Roumani
+
 			log.println((new Date()).toString() + "| Connection |" + client.getInetAddress());
-			
-			Worker worker = (new Worker(client)); // An object that does some work in on eor more background threads.
+
+			Worker worker = new Worker(client, this);
 			worker.handle();
+			
 			log.println((new Date()).toString()+ "| Disconnected |" + client.getInetAddress());
 		}
-		
 		server.close();
 		
 	}
 	
+	public void addToWhitelist(InetAddress IP) {
+		this.whitelist.add(IP);
+	}
+	
+	public void removeFromWhitelist(InetAddress IP) {
+		this.whitelist.remove(IP);
+	}
+		
 	public static void main(String[] args) throws Exception {
 		File f = new File("/eecs/home/ragheban/eclipse-workspace/EECS4413/src/a/running.txt");
-		PrintStream log = new PrintStream(f);
+		File l = new File("/eecs/home/ragheban/eclipse-workspace/EECS4413/src/a/log.txt");
+		PrintStream log = new PrintStream(l);
 		int port = 1024;
-		String hostname = "localhost";
 		TCPServer s = new TCPServer(port, log, f); // I don't know what this is.
 		
 	}
--- src/a/Worker.java
+++ src/a/Worker.java
@@ -1,28 +1,77 @@
 package a;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Worker {
 	
-	Socket client;
+	private Socket client;
+	private TCPServer server;
+	private OutputStream os;
+	private OutputStreamWriter osw;
+	private BufferedWriter bw;
 	
-	public Worker(Socket client) {
+	public Worker(Socket client, TCPServer server) {
 		this.client = client;
+		this.server = server;
 	}
 
 	/*
 	 * Handle simply takes in the text that the client wants to send.
 	 */
 	public void handle() throws Exception {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Start writing: \n");System.out.println("Okay?");
-		String in = input.nextLine();
-		while (in.equals("Bye")) {
-			in = input.nextLine();
-			System.out.println(in);
+		InputStream is = client.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		os = client.getOutputStream();
+		osw = new OutputStreamWriter(os);
+		bw = new BufferedWriter(osw);
+		
+		String message = "";
+		while (!message.equals("Bye")) {
+			message = br.readLine();
+			System.out.println("Processing the request: " + message + "\n");
+			String response;
+			
+			if (message.equals("getTime")) {
+				response = this.getTime();
+				bw.write(response + "\n");
+				bw.flush();
+			} else if (message.equals("Bye")) {
+				response = "Goodbye!\n";
+				bw.write(response);
+				bw.flush();
+				this.Bye();
+			} else if (message.matches("Punch\\s([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])")) {
+				response = "Adding IP: " + message.substring(6) + " to the whitelist.\n";
+				bw.write(response);
+				bw.flush();
+				this.Punch(message.substring(6));
+			} else if (message.matches("Plug\\s([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])")) {
+				response = "Removing IP: " + message.substring(5) + " to the whitelist.\n";
+				bw.write(response);
+				bw.flush();
+				this.Plug(message.substring(5));
+			} else {
+				response = "Please enter a valid request!\n";
+				bw.write(response);
+				bw.flush();
+			}
 		}
 		this.Bye();
 	}
@@ -30,9 +79,56 @@ public class Worker {
 	/*
 	 * M1
 	 */
-	public void Bye() throws Exception {
-		
+	private String getTime() {
+		Date d = new Date();
+		return d.toString();
+	}
+
+	/*
+	 * M2
+	 */
+	private void Bye() throws Exception {
 		client.close();
 	}
+	
+	/*
+	 * M3
+	 */
+	private void Punch(String IP) throws Exception {
+		String patternString = "([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])";
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(IP);
+        if (matcher.matches()) {
+        	String response = "Done!\n";
+        	this.server.addToWhitelist(InetAddress.getByName(IP));
+        	bw.write(response);
+			bw.flush();
+        } else {
+        	String response = "Invalid IP! Not added.\n";
+        	bw.write(response);
+			bw.flush();
+        }
+		
+	}
+	
+	/*
+	 * M4
+	 */
+	private void Plug(String IP) throws Exception {
+		String patternString = "([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])\\.([0-2]?[0-5]?[0-5])";
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(IP);
+        if (matcher.matches()) {
+        	String response = "Done!\n";
+        	this.server.removeFromWhitelist(InetAddress.getByName(IP));
+        	bw.write(response);
+			bw.flush();
+        } else {
+        	String response = "Invalid IP! Not removed.\n";
+        	bw.write(response);
+			bw.flush();
+        }
+	}
+
 
 }
